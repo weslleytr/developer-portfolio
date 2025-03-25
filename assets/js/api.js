eaae8bf9e9981684dfd4021eeeb20790f77a0551
Weslley Araújo Trindade <116997181+weslleytr@users.noreply.github.com>
2025-03-25 19:12:45 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/weslleytr/js-developer-portfolio/refs/heads/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/weslleytr/developer-portfolio/refs/heads/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }
